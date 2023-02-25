01ff74a041bebff961a5bb36ccb007f31fd83696
Zijun <s410126080@gmail.com>
2023-02-25 20:59:28 +0800

    improved maxSum

--- sliding window.js
+++ sliding window.js
@@ -1,26 +1,49 @@
-function MaxSum(arr, size) {
-  let MaxSum = 0;
-  for (let i = 0; i <= arr.length - size; i++) {
-    let tempSum = 0;
-    for (let j = i; j < i + size; j++) {
-      tempSum = tempSum + arr[j];
-    }
-    if (tempSum > MaxSum) MaxSum = tempSum;
+function maxSum(arr, size) {
+  let maxSum = 0;
+
+  for (let i = 0; i < size; i++) {
+    maxSum += arr[i];
   }
-  return MaxSum;
+
+  let previousSum = maxSum;
+
+  for (let i = 1; i <= arr.length - size; i++) {
+    let tempSum = 0;
+
+    tempSum = previousSum - arr[i - 1] + arr[i + size - 1];
+
+    if (tempSum > maxSum) {
+      maxSum = tempSum;
+    }
+
+    previousSum = tempSum;
+  }
+
+  return maxSum;
 }
 
-function MinSum(arr, size) {
-  let MinSum = 0;
-  for (let i = 0; i <= arr.length - size; i++) {
-    let tempSum = 0;
-    for (let j = i; j < i + size; j++) {
-      tempSum += arr[j];
-    }
-    if (tempSum < MinSum) MinSum = tempSum;
+function minSum(arr, size) {
+  let minSum = 0;
+
+  for (let i = 0; i < size; i++) {
+    minSum += arr[i];
   }
-  return MinSum;
+
+  let previousSum = minSum;
+
+  for (let i = 1; i <= arr.length - size; i++) {
+    let tempSum = 0;
+    tempSum = previousSum - arr[i - 1] + arr[i + size - 1];
+
+    if (tempSum < minSum) {
+      minSum = tempSum;
+    }
+
+    previousSum = tempSum;
+  }
+
+  return minSum;
 }
 
-// console.log(MaxSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3)); // 12
-console.log(MinSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3)); // -28
+console.log(maxSum([2, 7, 3, 7, 25, 0, 6, 1, -5, -12, -11], 3)); // 35
+console.log(minSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3)); // -28
